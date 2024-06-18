e7360e82af56bb9d22282d2da3ce164c44c3959a
omkar-2483 <kastureomkar03@gmail.com>
2024-06-18 19:03:00 +0530

    .

--- LB-3-Flask/app.py
+++ LB-3-Flask/app.py
@@ -9,3 +9,17 @@ def hello():
   
 if __name__=='__main__': 
    app.run()
+
+from flask import Flask 
+app = Flask(__name__) 
+
+@app.route('/blog/<postID>') 
+def show_blog(postID): 
+    return 'Blog Number %d' % postID 
+
+@app.route('/rev/<revNo>') 
+def revision(revNo): 
+    return 'Revision Number %f' % revNo 
+
+if __name__ == '__main__': 
+app.run() 
